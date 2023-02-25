94c1a733a901c9ed366d145e9af84dba822e73bb
cemgthedev <cemg.the.dev@gmail.com>
2023-02-25 10:48:21 -0300

    configurações iniciais do projeto e criação da interface Profile

---
+++ app/src/App.tsx
@@ -0,0 +1,7 @@
+import './styles/global.css'
+
+export function App() {
+  return (
+    <h1 className='text-red-600 text-2xl'>Testando Tailwindcss</h1>
+  );
+}
---
+++ app/src/profile/Profile.ts
@@ -0,0 +1,39 @@
+export interface Education {
+    university: string,
+    course: string,
+    period: string
+}
+
+export interface Technology {
+    imageUrl: string,
+    label: string
+}
+
+export interface Skill {
+    label: string,
+    level: string,
+    percentageRange: number
+}
+
+export interface Project {
+    imageUrl: string,
+    label: string,
+    githubProjectUrl: string,
+    siteProjectUrl: string,
+    description: string
+}
+
+export interface Contact {
+    imageUrl: string,
+    siteUrl: string
+}
+
+export interface Profile {
+    imageUrl: string,
+    bio: string,
+    educational: Education[],
+    stack: Technology[],
+    skills: Skill[],
+    projects: Project[],
+    contacts: Contact[]
+}
