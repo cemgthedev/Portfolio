6eacafeab3eaaccb1f3816686a345626241befb2
cemgthedev <cemg.the.dev@gmail.com>
2023-04-21 13:27:03 -0300

    configurando estilos de texto e animações no talwindcss

--- app/src/pages/LandingPage/index.tsx
+++ app/src/pages/LandingPage/index.tsx
@@ -1,4 +1,7 @@
+import { TypeAnimation } from 'react-type-animation';
+
 import "../../styles/global.css";
+import { Heading } from "../../components/Heading";
 
 export function LandingPage() {
     return (
@@ -9,7 +12,32 @@ export function LandingPage() {
                 h-screen
             "
         >
-            Home
+            <div
+                className="
+                    min-h-[96px]
+                    text-xl 
+                    font-semibold 
+                    flex 
+                    flex-col 
+                    items-center
+                "
+            >
+            <span>
+                Eu sou&nbsp;
+            </span>
+            <TypeAnimation
+                sequence={[
+                "Desenvolvedor Front End",
+                2000,
+                "Universitário",
+                2000
+                ]}
+                speed={50}
+            wrapper="span"
+                repeat={Infinity}
+                className='text-purple-500 uppercase'
+            />
+            </div>
         </div>
     );
 }
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -5,10 +5,49 @@ export default {
   ],
   theme: {
     extend: {
+      fontSize: {
+        '2xl': '28px',
+        'xl': '24px',
+        'lg': '20px',
+        'md': '16px',
+        'sm': '12px',
+      },
       fontFamily: {
-        sans: ['Roboto', 'sans'],
+        sans: ['Roboto', 'sans']
+      },
+      keyframes: {
+        top: {
+          '0%': {
+            transform: 'translateY(50px)',
+            opacity: 0
+          }
+        },
+        rigth: {
+          '0%': {
+            transform: 'translateX(-100px)',
+            opacity: 0
+          },
+        },
+        bottom: {
+          '0%': {
+            transform: 'translateY(-50px)',
+            opacity: 0
+          }
+        },
+        left: {
+          '0%': {
+            transform: 'translateX(100px)',
+            opacity: 0
+          }
+        }
+      },
+      animation: {
+        top: 'top 450ms ease-in-out',
+        rigth: 'rigth 750ms ease-in-out',
+        bottom: 'bottom 450ms ease-in-out',
+        left: 'left 750ms ease-in-out'
       }
-    },
+    }
   },
   plugins: [
     require("daisyui"),
