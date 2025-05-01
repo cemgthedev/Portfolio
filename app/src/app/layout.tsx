55dfe2357486cf13922f7914ae8df94c19efb730
cemgthedev <cemg.the.dev@gmail.com>
2025-05-01 14:17:07 -0300

    feat: testando script de instalação do JivoChat

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,25 +1,28 @@
-import '@/styles/globals.css'
-import type { Metadata } from 'next'
-import { Roboto } from 'next/font/google'
+import "@/styles/globals.css";
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
 
 const roboto = Roboto({
-  subsets: ['latin'],
-  weight: ['100', '300', '400', '500', '700'],
-})
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700"],
+});
 
 export const metadata: Metadata = {
-  title: 'CEMG THE DEV',
-  description: 'Site de portfólio',
-}
+  title: "CEMG THE DEV",
+  description: "Site de portfólio",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <script src="//code.jivosite.com/widget/D2w4SPR7yx" async></script>
+      </head>
       <body className={`${roboto.className}`}>{children}</body>
     </html>
-  )
+  );
 }
