57fbdd84a2ecb245d46418e5fb5fdb11f4134537
cemgthedev <cemg.the.dev@gmail.com>
2025-05-11 16:55:05 -0300

    refactor: atualizando descrição na dashboard

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,10 +1,10 @@
-'use client'
-import { BannerTitle } from '@/components/BannerTitle'
-import { Card } from '@/components/Card'
-import { CustomLink } from '@/components/CustomLink'
-import { Figure } from '@/components/Figure'
-import { Label } from '@/components/Label'
-import { Text } from '@/components/Text'
+"use client";
+import { BannerTitle } from "@/components/BannerTitle";
+import { Card } from "@/components/Card";
+import { CustomLink } from "@/components/CustomLink";
+import { Figure } from "@/components/Figure";
+import { Label } from "@/components/Label";
+import { Text } from "@/components/Text";
 
 import {
   IconAddressBook,
@@ -12,22 +12,22 @@ import {
   IconGithub,
   IconLinkedin,
   IconScroll,
-  IconWhatsapp
-} from '@/assets/icons'
-import { contacts } from '@/model/profile'
-import Image from 'next/image'
+  IconWhatsapp,
+} from "@/assets/icons";
+import { contacts } from "@/model/profile";
+import Image from "next/image";
 export default function Home() {
   return (
     <Card className="min-h-screen w-screen bg-zinc-950 p-4 text-white sm:p-8">
       <aside className="flex w-full py-4">
-        <Figure className="w-full m-auto animate-top opacity-0 px-0 gap-4">
+        <Figure className="m-auto w-full animate-top gap-4 px-0 opacity-0">
           <BannerTitle
             mainText="Eu sou"
             listOfText={[
-              'Desenvolvedor Front End', 
-              2000, 
-              'Desenvolvedor Mobile',
-              2000
+              "Desenvolvedor Front End",
+              2000,
+              "Desenvolvedor Mobile",
+              2000,
             ]}
             className="uppercase text-violet-500"
           />
@@ -60,18 +60,21 @@ export default function Home() {
           <figcaption className="flex w-[60vw] flex-col text-center max-md:w-[75vw]">
             <Text>Olá, me chamo Carlos Eduardo de Moura Gomes.</Text>
             <Text>
-              Desenvolvo o Front End de aplicações web e mobile por meio de conhecimentos em UI Design 
-              e tecnologias como Figma, React, TypeScript, Kotlin e Jetpack Compose.
+              Desenvolvedor de aplicações web e mobile com foco em UI Design e
+              tecnologias como Figma, React, React Native e TypeScript.
             </Text>
           </figcaption>
           <div className="flex w-fit gap-4">
             <CustomLink
               href="/portfolio"
               rounded="full"
-              shadow='secondary'
-              className="animate-right opacity-0 px-6 py-2"
+              shadow="secondary"
+              className="animate-right px-6 py-2 opacity-0"
             >
-              <IconAddressBook size={28} className='min-h-[28px] min-w-[28px]' />
+              <IconAddressBook
+                size={28}
+                className="min-h-[28px] min-w-[28px]"
+              />
               <Label>Portfólio</Label>
             </CustomLink>
             <CustomLink
@@ -79,9 +82,9 @@ export default function Home() {
               rounded="full"
               style="secondary"
               shadow="primary"
-              className="animate-left opacity-0 px-6 py-2"
+              className="animate-left px-6 py-2 opacity-0"
             >
-              <IconScroll size={28} className='min-h-[28px] min-w-[28px]' />
+              <IconScroll size={28} className="min-h-[28px] min-w-[28px]" />
               <Label>Currículo</Label>
             </CustomLink>
           </div>
@@ -90,33 +93,33 @@ export default function Home() {
           <CustomLink
             href={`https://github.com/${contacts.github}`}
             next={false}
-            className="animate-surgir opacity-0 h-[32px] w-[32px]"
+            className="h-[32px] w-[32px] animate-surgir opacity-0"
           >
-            <IconGithub size={28} className='min-h-[28px] min-w-[28px]' />
+            <IconGithub size={28} className="min-h-[28px] min-w-[28px]" />
           </CustomLink>
           <CustomLink
             href={`https://www.linkedin.com/in/${contacts.linkedin}`}
             next={false}
-            className="animate-surgirtwo opacity-0 h-[32px] w-[32px]"
+            className="h-[32px] w-[32px] animate-surgirtwo opacity-0"
           >
-            <IconLinkedin size={28} className='min-h-[28px] min-w-[28px]' />
+            <IconLinkedin size={28} className="min-h-[28px] min-w-[28px]" />
           </CustomLink>
           <CustomLink
             href={`mailto:${contacts.email}`}
             next={false}
-            className="animate-surgirthree opacity-0 h-[32px] w-[32px]"
+            className="h-[32px] w-[32px] animate-surgirthree opacity-0"
           >
-            <IconEmail size={28} className='min-h-[28px] min-w-[28px]' />
+            <IconEmail size={28} className="min-h-[28px] min-w-[28px]" />
           </CustomLink>
           <CustomLink
             href={`http://api.whatsapp.com/send?phone=${contacts.whatsapp}`}
             next={false}
-            className="animate-surgirfour opacity-0 h-[32px] w-[32px]"
+            className="h-[32px] w-[32px] animate-surgirfour opacity-0"
           >
-            <IconWhatsapp size={28} className='min-h-[28px] min-w-[28px]'/>
+            <IconWhatsapp size={28} className="min-h-[28px] min-w-[28px]" />
           </CustomLink>
         </div>
       </aside>
     </Card>
-  )
+  );
 }
