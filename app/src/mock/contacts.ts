90ed76e929fd265dc703dfc6230fa909f866693f
cemgthedev <cemg.the.dev@gmail.com>
2025-11-14 11:02:02 -0300

    refactor: correção no link para perfil do linkedln

--- app/src/mock/contacts.ts
+++ app/src/mock/contacts.ts
@@ -1,10 +1,6 @@
-import { TCertificate } from "@/types/TCertificate";
-import { TExperience } from "@/types/TExperience";
-import { TSkill } from "@/types/TSkill";
-
 export const contacts = {
   github: "cemgthedev",
-  linkedin: "carlos-eduardo-moura-gomes-bb0ab7250",
+  linkedin: "cemgdev",
   email: "cemg.the.dev@gmail.com",
   whatsapp: "5585997981087",
 };
